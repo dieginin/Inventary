6d8eb62d33d79482f26b80336c2c64cccb6888d4
dieginin <diegobalestra9@gmail.com>
2023-11-18 20:40:05 -0500

    missing sizes added

--- main.py
+++ main.py
@@ -4,46 +4,41 @@ import pandas as pd
 
 from models import Policy
 
-szs = ["xxs", "xs", "s", "m", "l", "xl", "1x", "2x", "3x", "4x", "5x", ""]
+szs = ["xxs", "xs", "s", "m", "l", "xl", "1x", "2x", "3x", "4x", "5x"]
 
 with open("config/policy.json", "r") as fl:
     policy = Policy(json.load(fl))
 df = pd.read_csv("data/data.csv")
 
 
-def find_colors(brand):
-    return policy.scrubs.brands.get(brand, {})
-
-
-def find_max_size(color):
-    return policy.scrubs.colors.get(color, {})
-
-
-def desire_count(length, size):
-    return policy.scrubs.sizes.get(length, {}).get(size)
-
-
-def get_missing(obj):
-    missing = {}
-
+def get_missing_style(obj):
     sub = df[df["CODE"] == obj]
     brand = sub["BRAND"].iloc[0]
-    pol_colors = policy.scrubs.brands.get(brand, {})
 
-    for color in pol_colors:
+    missing_colors = {}
+    for color in policy.scrubs.colors(brand):
         if color not in sub["COLOR"].unique():
-            color_dict = {}
-            for length in policy.scrubs.get_sizes():
-                size_dict = {}
-                for size in szs[:-1]:
-                    desire = desire_count(length, size)
-                    if desire:
-                        size_dict[size] = desire
-                color_dict[length] = size_dict
-            missing[color] = color_dict
+            missing_colors[color] = {
+                length: {
+                    size: policy.scrubs.desire(length, size)
+                    for size in szs
+                    if policy.scrubs.desire(length, size)
+                }
+                for length in policy.scrubs.get_sizes()
+            }
 
-    return missing
+    missing_sizes = {}
+    for _, r in sub.iterrows():
+        length, size, color, count = r["LENGTH"], r["SIZE"], r["COLOR"], r["COUNT"]
+        desire = policy.scrubs.desire(length, size)
+
+        if desire > count:
+            color_dict = missing_sizes.setdefault(color, {})
+            length_dict = color_dict.setdefault(length, {})
+            length_dict[size] = desire - count
+
+    return {**missing_colors, **missing_sizes}
 
 
-get_missing("7726")
-# print(get_missing("7726"))
+# get_missing("7726")
+print(get_missing_style("7726"))
